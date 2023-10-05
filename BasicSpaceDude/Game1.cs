f2ef2066bab73ede89f79fa0ec24fd84c8b03488
Kylesneddon4 <Kylesneddon4@gmail.com>
2023-10-05 10:05:30 +0100

    Switch to array implementation

--- BasicSpaceDude/Game1.cs
+++ BasicSpaceDude/Game1.cs
@@ -15,7 +15,7 @@ namespace BasicSpaceDude
         Background bg;
 
         // An array of dudes
-        FloatyDude dude;
+        FloatyDude[] dudes;
 
         public Game1()
         {
@@ -26,6 +26,8 @@ namespace BasicSpaceDude
 
         protected override void Initialize()
         {
+            //Specify that dudes have space for 100 dudes
+            dudes = new FloatyDude[100];
             base.Initialize();
         }
 
@@ -39,12 +41,12 @@ namespace BasicSpaceDude
                                                     GraphicsDevice.Viewport.Bounds.Center.Y + RNG.Next(-100, 100));
             Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
                                                     (float)(RNG.NextDouble() * 2) - 1);
-            dude = new FloatyDude(Content.Load<Texture2D>("dude0"), startPos, startVel);
+            dudes[0] = new FloatyDude(Content.Load<Texture2D>("dude0"), startPos, startVel);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            dude.UpdateMe(GraphicsDevice.Viewport.Bounds);
+            dudes[0].UpdateMe(GraphicsDevice.Viewport.Bounds);
 
             base.Update(gameTime);
         }
@@ -56,7 +58,7 @@ namespace BasicSpaceDude
             _spriteBatch.Begin();
             bg.DrawMe(_spriteBatch);
 
-            dude.DrawMe(_spriteBatch);
+            dudes[0].DrawMe(_spriteBatch);
 
             _spriteBatch.End();
 
